ffdbc48a9c1c576491457db9d5930887f290d7fa
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-06-08 18:22:45 -0400

    make panel scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,40 @@ export default function Home() {
   return (
     <main>
       <div className="flex h-screen text-white">
-        <div className="bg-gray-800 p-4">
-          <div className="flex aspect-square w-12 items-center justify-center rounded-full bg-white text-gray-800">
-            TW
+        <div className="space-y-2 overflow-y-scroll bg-gray-800 p-3">
+          {[...Array(40)].map((_, i) => (
+            <div
+              key={i}
+              className="flex aspect-square w-12 items-center justify-center rounded-full bg-white text-gray-800"
+            >
+              {i}
+            </div>
+          ))}
+        </div>
+
+        <div className="flex w-60 flex-col bg-gray-700">
+          <div className="flex h-12 items-center px-3 shadow-md">
+            Tailwind CSS
+          </div>
+          <div className="flex-1 space-y-2 overflow-y-scroll p-3">
+            {[...Array(40)].map((_, i) => (
+              <p key={i}>channel {i}</p>
+            ))}
           </div>
         </div>
-        <div className="flex w-60 flex-col bg-gray-700">
-          <div className="p-4 shadow-md">Tailwind CSS</div>
-          <div className="grow p-4">channels</div>
-        </div>
-        <div className="flex grow flex-col bg-gray-600">
-          <div className="p-4 shadow-md">general</div>
-          <div className="grow p-4">messages</div>
+
+        <div className="flex flex-1 flex-col bg-gray-600">
+          <div className="flex h-12 items-center px-3 shadow-md">general</div>
+          <div className="flex-1 space-y-4 overflow-y-scroll p-3">
+            {[...Array(40)].map((_, i) => (
+              <p key={i}>
+                Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Asperiores, quam quod! Dicta voluptas omnis vel debitis
+                libero obcaecati ex earum harum laborum in nostrum odit, quos
+                aliquid esse deleniti officia.
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </main>
