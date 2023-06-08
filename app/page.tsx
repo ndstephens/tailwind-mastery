6316844706b7be7689122779499d81e1c44483f9
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-06-08 18:08:05 -0400

    put the multipanel work on the index

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-700 text-white">
-      <div className="flex flex-col gap-y-2">
-        <Link href="/discord-messages">Discord Messages</Link>
-        <Link href="/multipanel">Multipanel Layout</Link>
+    <main>
+      <div className="flex h-screen text-white">
+        <div className="bg-gray-800 p-4">
+          <div className="flex aspect-square w-12 items-center justify-center rounded-full bg-white text-gray-800">
+            TW
+          </div>
+        </div>
+        <div className="flex w-60 flex-col bg-gray-700">
+          <div className="p-4 shadow-md">Tailwind CSS</div>
+          <div className="grow p-4">channels</div>
+        </div>
+        <div className="flex grow flex-col bg-gray-600">
+          <div className="p-4 shadow-md">general</div>
+          <div className="grow p-4">messages</div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
