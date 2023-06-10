942243e3aff1b79d0e1906385f44ae3b9047ca1d
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-06-10 18:37:34 -0400

    add channels

--- app/servers/[sid]/page.tsx
+++ app/servers/[sid]/page.tsx
@@ -1,8 +1,10 @@
 import {
   AddPersonIcon,
+  ArrowIcon,
   BookIcon,
   CheckIcon,
   ChevronIcon,
+  HashtagIcon,
   SpeakerphoneIcon,
   VerifiedIcon,
 } from '@/components/icons';
@@ -18,7 +20,7 @@ export default function Server({ params }: { params: { sid: string } }) {
 
   return (
     <>
-      {/* CHANNELS */}
+      {/* CHANNELS SECTION */}
       <div className="flex w-60 flex-col bg-gray-800">
         {/* Header */}
         <button className="flex h-12 items-center px-4 font-title text-[15px] font-semibold text-white shadow-sm transition hover:bg-gray-550/[0.16]">
@@ -29,10 +31,11 @@ export default function Server({ params }: { params: { sid: string } }) {
           {server.title}
           <ChevronIcon className="ml-auto aspect-square w-[18px] opacity-80" />
         </button>
-        {/* Channels */}
+
+        {/* Channels List */}
         <div className="mt-[17px] flex-1 overflow-y-scroll font-medium text-gray-300">
+          {/* welcome / announcements */}
           <div className="space-y-0.5">
-            {/* welcome */}
             <Link
               href="#"
               className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
@@ -41,7 +44,6 @@ export default function Server({ params }: { params: { sid: string } }) {
               welcome
               <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
             </Link>
-            {/* announcements */}
             <Link
               href="#"
               className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
@@ -51,10 +53,52 @@ export default function Server({ params }: { params: { sid: string } }) {
               <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
             </Link>
           </div>
+          {/* Category */}
+          <div className="mt-[21px]">
+            <button className="flex items-center px-0.5 font-title text-xs uppercase tracking-wide">
+              <ArrowIcon className="mr-0.5 aspect-square w-3" />
+              Tailwind CSS
+            </button>
+
+            <div className="mt-[5px] space-y-0.5">
+              <Link
+                href="#"
+                className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
+              >
+                <HashtagIcon className="mr-1.5 aspect-square w-5 text-gray-400" />
+                general
+                <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+              </Link>
+              <Link
+                href="#"
+                className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
+              >
+                <HashtagIcon className="mr-1.5 aspect-square w-5 text-gray-400" />
+                plugins
+                <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+              </Link>
+              <Link
+                href="#"
+                className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
+              >
+                <HashtagIcon className="mr-1.5 aspect-square w-5 text-gray-400" />
+                help
+                <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+              </Link>
+              <Link
+                href="#"
+                className="group mx-2 flex items-center rounded px-2 py-1 text-gray-300 hover:bg-gray-550/[0.16] hover:text-gray-100"
+              >
+                <HashtagIcon className="mr-1.5 aspect-square w-5 text-gray-400" />
+                internals
+                <AddPersonIcon className="ml-auto aspect-square w-4 text-gray-200 opacity-0 hover:text-gray-100 group-hover:opacity-100" />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* MESSAGES */}
+      {/* MESSAGES SECTION */}
       <div className="flex flex-1 flex-col bg-gray-700">
         {/* Header */}
         <div className="flex h-12 items-center px-3 shadow-sm">general</div>
