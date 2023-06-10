f2a7a00615d15db23710967c817e77585453818c
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-06-10 13:42:09 -0400

    basic server page

--- app/servers/1/page.tsx
+++ app/servers/1/page.tsx
@@ -1,3 +1,31 @@
-export default function Server1() {
-  return <p className="text-black">Server 1</p>;
+export default function Server() {
+  return (
+    <>
+      <div className="flex w-60 flex-col bg-gray-800">
+        <div className="flex h-12 items-center px-3 font-title font-semibold text-white shadow-md">
+          Server 1
+        </div>
+        <div className="flex-1 space-y-2 overflow-y-scroll p-3 font-medium text-gray-300">
+          <p className="text-white">general</p>
+          {[...Array(40)].map((_, i) => (
+            <p key={i}>channel {i}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-1 flex-col bg-gray-700">
+        <div className="flex h-12 items-center px-3 shadow-md">general</div>
+        <div className="flex-1 space-y-4 overflow-y-scroll p-3">
+          {[...Array(40)].map((_, i) => (
+            <p key={i}>
+              Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Asperiores, quam quod! Dicta voluptas omnis vel debitis
+              libero obcaecati ex earum harum laborum in nostrum odit, quos
+              aliquid esse deleniti officia.
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
