28e077f46a96ee4fed0ad8580e5722b6f5b19cfb
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2023-06-13 14:13:21 -0400

    add search input

--- app/servers/[sid]/channels/[cid]/page.tsx
+++ app/servers/[sid]/channels/[cid]/page.tsx
@@ -104,11 +104,13 @@ export default function ChannelPage({ params }: PageProps) {
         <div className="flex h-12 items-center px-2 shadow-sm">
           <div className="flex items-center">
             <Icon className="mx-2 h-6 w-6 font-semibold text-gray-400" />
-            <span className="mr-2 font-title text-white">{channel?.label}</span>
+            <span className="mr-2 whitespace-nowrap font-title text-white">
+              {channel?.label}
+            </span>
           </div>
           {channel?.description && (
             <>
-              <div className="bg-white/{.06} mx-2 h-6 w-px" />
+              <div className="mx-2 h-6 w-px bg-white/[.06]" />
               <div className="mx-2 truncate text-sm font-medium text-gray-200">
                 {channel.description}
               </div>
@@ -127,6 +129,16 @@ export default function ChannelPage({ params }: PageProps) {
             <button className="text-gray-200 hover:text-gray-100">
               <Icons.People className="mx-2 h-6 w-6" />
             </button>
+            <div className="relative mx-2">
+              <input
+                type="text"
+                placeholder="Search"
+                className="h-6 w-36 rounded border-none bg-gray-900 px-1.5 text-sm font-medium placeholder-gray-400"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center">
+                <Icons.Spyglass className="mr-1.5 aspect-square w-4 text-gray-400" />
+              </div>
+            </div>
             <button className="text-gray-200 hover:text-gray-100">
               <Icons.Inbox className="mx-2 h-6 w-6" />
             </button>
